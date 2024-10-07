aaa5437f6aa1d0b4cf67d1d7ff3696dd0f36d26a
TimGathura <timgathura@gmail.com>
2024-10-07 12:13:44 +0300

    Log in done

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  }
+
+
+
+  // "build": "tailwindcss -i static/css/app.css -o static/css/output.css",
+    // "watch": "tailwindcss -i static/css/app.css -o static/css/output.css --watch",
